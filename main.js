8da14a7fca53fdf950db73f076bace377230e47f
PamelaSCB <pamiboyle@gmail.com>
2023-07-09 19:47:10 +0200

    Nivel 1 Ejercicio 3

--- main.js
+++ main.js
@@ -1,16 +1,55 @@
-"use strict";
+let chiste;
+
+const mostrarBtnes = () => {
+  document
+    .querySelectorAll("[puntuacion]")
+    .forEach((boton) => (boton.style.display = "block"));
+};
+const esconderBtnes = () => {
+  document
+    .querySelectorAll("[puntuacion]")
+    .forEach((boton) => (boton.style.display = "none"));
+};
 
 const respuestaChiste = async () => {
+  esconderBtnes();
   try {
-    const respuesta = await fetch("https://icanhazdadjoke.com/", {
+    const url = "https://icanhazdadjoke.com/";
+    const opciones = {
       headers: {
-        Accept: "application/json",
+        accept: "application/json",
       },
-    });
+    };
+    const respuesta = await fetch(url, opciones);
     const json = await respuesta.json();
-    console.log("Chiste: ", json.joke);
-    document.querySelector("#text-chiste").innerHTML = json.joke;
+    chiste = json.joke;
+
+    console.log("Chiste: ", chiste);
+
+    document.querySelector("#text-chiste").innerHTML = `" ${chiste} "`;
   } catch (err) {
     console.log(err.message);
   }
+
+  mostrarBtnes();
+};
+
+const reporteChistes = [];
+
+const puntuacion = (score) => {
+  console.log("puntuacion");
+  let aLen = reporteChistes.length;
+  let ultimoChiste = reporteChistes[aLen - 1];
+
+  if (aLen > 0 && ultimoChiste.joke === chiste) {
+    ultimoChiste.score = score;
+    ultimoChiste.date = new Date().toISOString();
+  } else {
+    reporteChistes.push({
+      joke: chiste,
+      score: score,
+      date: new Date().toISOString(),
+    });
+  }
+  console.table(reporteChistes);
 };
